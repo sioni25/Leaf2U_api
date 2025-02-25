5fcf5a105d9d315bad529e9e139aa5aa877d12d4
moonkyoungmi <cher1605@naver.com>
2025-02-25 12:14:59 +0900

    init

---
+++ src/main/java/kr/co/leaf2u_api/Leaf2UApiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.leaf2u_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leaf2UApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Leaf2UApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/leaf2u_api/Leaf2UApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.leaf2u_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Leaf2UApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
